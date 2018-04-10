ca307f03a7667afc7c2e37ce9712e527cbd83fb0
Trifon Triantafillidis <trifont@otenet.gr>
2018-04-10 17:37:14 +0300

    Reorganized vs solution and project

--- gealib/chromosome.cpp
+++ gealib/chromosome.cpp
@@ -3,19 +3,16 @@
 using namespace gealib;
 
 // Parameterless constructor
-
 chromosome::chromosome() : vector<bool>()
 {
 }
 
 // Creates a chromosome of specidied size
-
 chromosome::chromosome(size_t size) : vector<bool>(size)
 {
 }
 
 // Encodes an area of the chromosome as integer
-
 longint chromosome::get(size_t pos, size_t bits)
 {
 	longint val = 0;
@@ -27,7 +24,6 @@ longint chromosome::get(size_t pos, size_t bits)
 }
 
 // Sets an area of the chromosome decoding an integer
-
 void chromosome::set(size_t pos, size_t bits, longint value)
 {
 	longint bit = 1;
@@ -36,7 +32,6 @@ void chromosome::set(size_t pos, size_t bits, longint value)
 }
 
 // Flips a single bit at index location
-
 void chromosome::flip(size_t index)
 {
 	this->at(index) = !this->at(index);
@@ -53,7 +48,6 @@ void chromosome::randomize()
 }
 
 // Converts chromosome to a string of 0/1
-
 std::string chromosome::to_string()
 {
 	std::stringstream ss;
